dfdf08ccb9158a53fbeb894abf7e54b694bc742c
mosorio22 <moso4410@gmail.com>
2022-04-12 18:36:10 -0400

    switch to spaces/tabs. add weather map url to data

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-	<header id="weather-header">
-		<h1>Your Weather App</h1>
-	</header>
+   <header id="weather-header">
+      <h1>Your Weather App</h1>
+   </header>
 </template>
 
 <script>
 
 export default {
-	name: 'App'
+   name: 'App',
+   data() {
+      return {
+         api_key: "",
+         url_: "https://api.openweathermap.org/data/2.5/",
+      };
+   }
 }
 </script>
 
 <style lang="scss">
 html { 
-	background: url(assets/summerbeach.jpg) no-repeat center center fixed; 
-	background-size: cover;
-	@media (min-width: 768px) {
-		background-size:50%;
-		background-color:seashell;
-	}
+   background: url(assets/summerbeach.jpg) no-repeat center center fixed; 
+   background-size: cover;
+   @media (min-width: 768px) {
+      background-size:50%;
+      background-color:seashell;
+   }
 }
 </style>
