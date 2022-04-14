099d0f8d9e02a976acf60af86cd3b6629feacf64
mosorio22 <moso4410@gmail.com>
2022-04-13 21:54:53 -0400

    round temp to nearest 10th and display

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
       <h1>Your Weather App</h1>
    </header>
    <div>
-      {{  weatherData  }}
+      {{  temp  }}
    </div>
 </template>
 
@@ -15,7 +15,7 @@ export default {
    name: 'App',
    data() {
       return {
-         weatherData: null,
+         temp: null,
          url: "https://api.openweathermap.org/data/2.5/weather?",
          appid: ""
       };
@@ -25,10 +25,11 @@ export default {
       //var to hold component data scope
       let that = this;
       //get lat and long and pass into weather api
+      //save data in Farenheit (from Kelvin)
       navigator.geolocation.getCurrentPosition(function(position) {
          axios
          .get(that.url + "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=" + that.appid)
-         .then(response => (window.weatherData = response))
+         .then(response => (that.temp = ((response.data.main.temp - 273.15) * 9/5 + 32).toFixed(1)))
       });
    }
 }
